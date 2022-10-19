c335e2a8fddf4f60ee1c6f2973a9ef9c31a83698
vagrant <nick.james.joe@gmail.com>
2022-10-19 02:33:25 +0000

    creating a function that compare 2 sets of arrays and return if it is true or false

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+  let result;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result = true;
+    } else {
+      return false;
+    }
+
+  }
+  return result;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+
