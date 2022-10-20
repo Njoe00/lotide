cc2d30a471be7386ff9d2f47f26b620d2d46bc17
Njoe00 <nick.james.joe@gmail.com>
2022-10-20 09:37:19 -0700

    using objects and keys to find key value pairs

---
+++ findKeyValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(object, object2) {
+  if (findKeyByValue(object, object2)) {
+    console.log(`✅ Assertion Passed: ${object} === ${object2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${object} !== ${object2}`);
+  }
+};
+
+const findKeyByValue =  function(object, object2) {
+  for (const key in object) {
+    if (object[key] === object2) {
+      return [key];
+    }
+  }
+  return undefined;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
