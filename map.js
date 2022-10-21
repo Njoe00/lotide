b1d86287efbc1ba5bd40b8b5a34025c502cfb7bf
Njoe00 <nick.james.joe@gmail.com>
2022-10-20 21:20:02 -0700

    using maps to callback the first letter of each word

---
+++ map.js
@@ -0,0 +1,12 @@
+const map = function (array, callBack) {
+    const results = [];
+    for(let item of array) {
+        results.push(callBack(item));
+    }
+    return results; 
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
