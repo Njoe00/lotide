f8e78119a1ed0201e1835a67eb412e0edc97125a
Njoe00 <nick.james.joe@gmail.com>
2022-10-21 22:24:14 -0700

    creating a function that can find count the number of letters of a stirng

---
+++ countLetter.js
@@ -0,0 +1,31 @@
+
+function countLetters(str) {
+  var noSpaces = str.split(" ").join("");
+  console.log(noSpaces);
+  charCount = {};
+
+  for (var i= 0; i < noSpaces.length; i++) {
+    if (charCount[noSpaces[i]]) {
+      charCount[noSpaces[i]] += 1;
+    } else {
+      charCount[noSpaces[i]] = 1;
+      }
+
+  }
+      console.log(charCount);
+}
+
+
+
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑 Aseertion Failed: ${actual} !== ${expected}`);
+    }
+    
+  };
+
+assertEqual(countLetters('LHL'));
+assertEqual(countLetters("lighthouse in the house"));
