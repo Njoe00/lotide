6cf1417d9c4df81f7c57247c1f601a40e47f545d
Njoe00 <nick.james.joe@gmail.com>
2022-10-24 19:01:02 -0700

    update function to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,13 @@
+const assert = require('chai').assert;
 const head = require('../head.js');
 
+describe("head", () => {
+    it("return 1 for [1, 2, 3]", () => {
+        assert.strictEqual(head([1, 2, 3]), 1);
+    });
+    it("returns '5' for ['5']", () => {
+        assert.strictEqual(head(['5']), '5');
+    });
+});
 
-console.log((head([])));
-console.log(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+
