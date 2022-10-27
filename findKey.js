0d71b47c81f2344a2e8303576dadd24d7f28f642
Njoe00 <nick.james.joe@gmail.com>
2022-10-26 19:16:25 -0700

    updated function to take in value as a function

--- findKey.js
+++ findKey.js
@@ -1,23 +1,24 @@
-// look through an object value and the first truthly 
-// return key of that key value pair 
-// else returned undefined 
-const findKey = function (keys, value) {
-    keyObj = (Object.keys(keys));
-    for(let obj of keyObj) {
-        if(keys[obj] === value) {
-        return obj;
-        }     
-    }  
+// look through an object value and the first truthly
+// return key of that key value pair
+// else returned undefined
+const findKey = function(keys, value) {
+  for (let val in keys) {
+    console.log('test1', keys[val], 'test2', value(keys[val]));
+    if (value(keys[val])) {
+      return val;
+    }
+  }
+  return 'sad boi';
 };
 
 console.log(findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri":   { stars: 3 },
-    "noma":      { stars: 2 },
-    "elBulli":   { stars: 3 },
-    "Ora":       { stars: 2 },
-    "Akelarre":  { stars: 3 }
-  }, 2 )) // => "noma"
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
 
 
-  module.exports = findKey;
+module.exports = findKey;
